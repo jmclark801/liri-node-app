89eb5c2b276ed64aeac3bae1a2202c6087479585
Jim Clark - work computer <jmclark801@yahoo.com>
2019-05-13 10:31:52 -0600

    Add Movie lookup

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log("Keys File Loaded");
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
-var axios = require("axios");
-var Spotify = require("node-spotify-api");
-
-var keys = require("./keys.js");
+const axios = require("axios");
+const Spotify = require("node-spotify-api");
+const keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var searchTerm = process.argv[3];
@@ -15,8 +14,66 @@ function getBandsInTown() {
         "/events?app_id=codingbootcamp"
     )
     .then(function(response) {
-      console.log(response);
+      console.log("\n\n\nYou Searched For: " + searchTerm);
+      console.log("\n******** Results *********\n\n");
+      console.log(response.data[0].venue.name);
+      console.log(response.data[0].venue.city);
+      console.log(response.data[0].venue.region);
+      console.log(response.data[0].datetime);
+      console.log("\n\n******** End *********\n\n\n");
     });
+}
+
+function getSpotifyInfo() {
+  spotify.search({ type: "track", query: searchTerm }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    console.log("\n\n\nYou Searched For: " + searchTerm);
+    console.log("\n******** Results *********\n\n");
+    console.log("Song Name - " + data.tracks.items[0].name);
+    console.log("Artist Name - " + data.tracks.items[0].album.artists[0].name);
+    console.log("Album Name - " + data.tracks.items[0].album.name);
+
+    if (data.tracks.items[0].preview_url) {
+      console.log("Preview URL - " + data.tracks.items[0].preview_url);
+    } else {
+      console.log('Preview URL - Unavailable for "' + searchTerm + '"');
+    }
+    console.log("\n\n******** End *********\n\n\n");
+  });
+}
+
+function getMovieInfo() {
+  axios
+    .get("http://omdbapi.com/?apikey=trilogy&s=" + searchTerm)
+    .then(function(response) {
+      console.log("\n\n\nYou Searched For: " + searchTerm);
+      console.log(
+        "\nThis Search Resulted in " + response.data.Search.length + " results"
+      );
+      console.log("\n******** Results *********\n\n");
+      response.data.Search.forEach(movie => {
+        console.log("Movie Title - " + movie.Title);
+        console.log(`Movie Year - ${movie.Year}`);
+        console.log(`Movie IMDB ID - ${movie.imdbID}`);
+        console.log("Media Type - " + movie.Type);
+        console.log("Poster URL - " + movie.Poster);
+        console.log("\n");
+      });
+      console.log("\n******** End *********\n\n");
+      // console.log(response.data);
+    });
+
+  // Requirements
+  // * Title of the movie.
+  // * Year the movie came out.
+  // * IMDB Rating of the movie.
+  // * Rotten Tomatoes Rating of the movie.
+  // * Country where the movie was produced.
+  // * Language of the movie.
+  // * Plot of the movie.
+  // * Actors in the movie.
 }
 
 switch (command) {
@@ -24,8 +81,10 @@ switch (command) {
     getBandsInTown();
     break;
   case "spotify-this-song":
+    getSpotifyInfo();
     break;
   case "movie-this":
+    getMovieInfo();
     break;
   case "do-what-it-says":
     break;

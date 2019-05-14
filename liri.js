e128e1af8d128b928ba7dbed1798957f8a75a94b
Jim Clark - work computer <jmclark801@yahoo.com>
2019-05-14 09:56:37 -0600

    Add log.txt where results are appended after each search

--- liri.js
+++ liri.js
@@ -2,7 +2,6 @@
 // - Handle multiple results and No Results for each search
 // - Get correct API for IMDB vs OMDB (OMDB doesn't have the fields that are required)
 // - If no song is provided, then your program will default to "The Sign" by Ace of Base
-// - Append search results to text file called log.txt
 // -- https://tutorialedge.net/javascript/nodejs/reading-writing-files-with-nodejs/
 
 require("dotenv").config();
@@ -15,6 +14,7 @@ const fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
+var logString = "";
 
 function getBandsInTown() {
   axios
@@ -24,40 +24,47 @@ function getBandsInTown() {
         "/events?app_id=codingbootcamp"
     )
     .then(function(response) {
-      console.log("\n\n\nYou Searched For: " + searchTerm);
-      console.log("\n******** Results *********\n\n");
+      logString +=
+        "\n\n\nYou Searched For: " +
+        searchTerm +
+        "\n******** Results *********\n\n";
       if (response.data[0] === undefined) {
-        console.log("*** There were no results for this search ***\n\n\n");
+        logString += "*** There were no results for this search ***\n\n\n";
       } else {
         var unformattedDate = response.data[0].datetime;
         var dateFormat = "MM/DD/YYYY hh:mmA";
         var formattedDate = moment(unformattedDate).format(dateFormat);
-        console.log("Venue Name - " + response.data[0].venue.name);
-        console.log("Venue City - " + response.data[0].venue.city);
-        console.log("Region - " + response.data[0].venue.region);
-        console.log("Event Date - " + formattedDate);
+        logString += "Venue Name - " + response.data[0].venue.name;
+        logString += "\nVenue City - " + response.data[0].venue.city;
+        logString += "\nRegion - " + response.data[0].venue.region;
+        logString += "\nEvent Date - " + formattedDate;
       }
-      console.log("\n\n******** End *********\n\n\n");
+      logString += "\n\n******** End *********\n\n\n";
+      console.log(logString);
+      logResults();
     });
 }
 
 function getSpotifyInfo() {
   spotify.search({ type: "track", query: searchTerm }, function(err, data) {
     if (err) {
-      return console.log("Error occurred: " + err);
+      logString += "Error occurred: " + err;
+      return console.log(logString);
     }
-    console.log("\n\n\nYou Searched For: " + searchTerm);
-    console.log("\n******** Results *********\n\n");
-    console.log("Song Name - " + data.tracks.items[0].name);
-    console.log("Artist Name - " + data.tracks.items[0].album.artists[0].name);
-    console.log("Album Name - " + data.tracks.items[0].album.name);
-
+    logString += "\n\n\nYou Searched For: " + searchTerm;
+    logString += "\n******** Results *********\n\n";
+    logString += "\nSong Name - " + data.tracks.items[0].name;
+    logString +=
+      "\nArtist Name - " + data.tracks.items[0].album.artists[0].name;
+    logString += "\nAlbum Name - " + data.tracks.items[0].album.name;
     if (data.tracks.items[0].preview_url) {
-      console.log("Preview URL - " + data.tracks.items[0].preview_url);
+      logString += "\nPreview URL - " + data.tracks.items[0].preview_url;
     } else {
-      console.log('Preview URL - Unavailable for "' + searchTerm + '"');
+      logString += '\nPreview URL - Unavailable for "' + searchTerm + '"';
     }
-    console.log("\n\n******** End *********\n\n\n");
+    logString += "\n\n******** End *********\n\n\n";
+    console.log(logString);
+    logResults();
   });
 }
 
@@ -65,20 +72,28 @@ function getMovieInfo() {
   axios
     .get("http://omdbapi.com/?apikey=trilogy&s=" + searchTerm)
     .then(function(response) {
-      console.log("\n\n\nYou Searched For: " + searchTerm);
-      console.log(
-        "\nThis Search Resulted in " + response.data.Search.length + " results"
-      );
-      console.log("\n******** Results *********\n\n");
+      logString +=
+        "\n\n\nYou Searched For: " +
+        searchTerm +
+        "\nThis Search Resulted in " +
+        response.data.Search.length +
+        " results" +
+        "\n******** Results *********\n\n";
       response.data.Search.forEach(movie => {
-        console.log("Movie Title - " + movie.Title);
-        console.log(`Movie Year - ${movie.Year}`);
-        console.log(`Movie IMDB ID - ${movie.imdbID}`);
-        console.log("Media Type - " + movie.Type);
-        console.log("Poster URL - " + movie.Poster);
-        console.log("\n");
+        logString +=
+          "Movie Title - " +
+          movie.Title +
+          `\nMovie Year - ${movie.Year}` +
+          `\nMovie IMDB ID - ${movie.imdbID}` +
+          "\nMedia Type - " +
+          movie.Type +
+          "\nPoster URL - " +
+          movie.Poster +
+          "\n\n";
       });
-      console.log("\n******** End *********\n\n");
+      logString += "\n******** End *********\n";
+      console.log(logString);
+      logResults();
     });
 }
 
@@ -90,8 +105,6 @@ function doWhatItSays() {
       const searchTermArray = data.split(",");
       command = searchTermArray[0];
       searchTerm = searchTermArray[1];
-      console.log(command);
-      console.log(searchTerm);
       executeRequest();
     }
   });
@@ -128,6 +141,12 @@ function executeRequest() {
         "This was not a valid option.  Please make a valid selection"
       );
   }
+}
+
+function logResults() {
+  fs.appendFile("log.txt", logString, "utf8", function(err) {
+    if (err) throw err;
+  });
 }
 
 function buildRequest() {
